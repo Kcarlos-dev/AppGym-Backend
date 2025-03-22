e562e5edc5bc8424b22609faafcfc1f47fc2f0db
Kcarlos <ca850075@gmail.com>
2025-03-22 16:18:39 -0300

    criacao do arquico db.js para configuracao da conexao com banco

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+//require('dotenv').config({ path: '../.env' })
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao MySQL:', err);
+        return;
+    }
+    console.log('Conectado ao MySQL com sucesso!');
+
+});
+
+module.exports = connection.connect()
